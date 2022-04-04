03a380d485cdbbd26680b2a9c35e564aba00fd03
Giovana <giovana.viana@aluno.ifsp.edu.br>
2022-04-04 17:48:29 -0300

    Create Home

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css";
+import Nav from "./Nav";
+import Logo from "./Logo";
+
+
+const Header = () => {
+    return (
+        <header classname="header-nav">
+            <Logo />
+            <Nav />
+        </header>
+    );
+};
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import "./Nav.css";
+
+const Nav = () => {
+    return(
+        <nav>
+        <ul>
+        <li>Home</li>
+        <li>Sobre</li>
+        <li>Entrar</li>
+        <li>Cadastrar-se</li>
+        </ul>
+        </nav>
+    );
+    };
+    export default Nav;
