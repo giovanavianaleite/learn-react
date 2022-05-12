746ddbf8ec9d92ac938a31b4de0574b221bb478b
Giovana Viana <giovana.viana@aluno.ifsp.edu.br>
2022-05-12 17:48:10 -0300

    atualização aula 12-05

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,23 @@
 import './Footer.css'
+import {FaFacebook, FaInstagram} from 'react-icons/fa'
 
 const Footer = () => {
   return (
     <div className='footer'>
-        <span>Footer</span>
+        <footer>
+          <p>Siga-nos nas Redes Sociais:</p>
+          <ul className='social_list'>
+            <li>
+              <FaFacebook />
+            </li>
+            <li>
+              <FaInstagram />
+            </li>
+          </ul>
+          <p className='direitos'>
+            <span>Direitos autorais Giovana Viana.</span>© Todos os direitos reservados.
+          </p>
+        </footer>
     </div>
   )
 }
